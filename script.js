10632568b63d767dc0bcbdd238cf3147dc6fbc35
Vytautas Latvys <v.latvys@gmail.com>
2017-11-16 20:45:01 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,56 @@
+$(document).ready(() => {
+
+  $('#submit').on('click', (e) => {
+    event.preventDefault()
+
+    let $search = $('#search');
+
+    $('.searchResultWrapper').empty();
+    getData($search.val());
+    $search.val('');
+  })
+
+  function getData(searchVal) {
+    $.ajax({
+      url: 'http://en.wikipedia.org/w/api.php',
+      data: {
+        action: 'query',
+        format: 'json',
+        generator: 'search',
+        gsrsearch: searchVal,
+        gsrlimit: 10,
+        prop: 'extracts',
+        exintro: '',
+        explaintext: '',
+        exsentences: 1,
+      },
+      dataType: 'jsonp',
+      success: loopThroughResults
+    });
+  }
+
+  function loopThroughResults(data) {
+    let results = data.query.pages;
+    Object.keys(results).forEach((result) => {
+      console.log(results[result]);
+      renderResults(
+        results[result].pageid,
+        results[result].title,
+        results[result].extract
+      );
+    })
+  }
+
+  function renderResults(pageId, title, description) {
+    let url = 'https://en.wikipedia.org/?curid=' + pageId;
+
+    $('.searchResultWrapper').append(
+      '<a href="' + url + '" target="_blank" class="searchResult">' +
+        '<h2>' + title + '</h2>' +
+        '<p>' + description + '</p>' +
+      '</a>'
+    )
+  }
+});
+
+
